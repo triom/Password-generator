1b46eabd062da9a8cdc11032433ea79fa41fc44c
triom <triomphantesonwa@gmail.com>
2022-03-22 14:09:24 +0100

    jenkins test error

--- pwdgenerator.py
+++ pwdgenerator.py
@@ -5,7 +5,7 @@ print('Welcome i can generate a password for you')
 maxlength = int(
     input('please enter the length you will need for your password\n'))
 
-Figures = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+Figures = ['0', '1', '2', '3', '4', , '5', '6', '7', '8', '9']
 
 Lower_case = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
               'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
